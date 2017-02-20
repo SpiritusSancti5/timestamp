234e7f722dd391757be31e6be01f817cb15d4f6a
Sergiu Lucec <dreagsharr@yahoo.de>
2017-02-20 21:38:44 +0000

    probably final ver

--- server.js
+++ server.js
@@ -1,1 +1,39 @@
-var express = require ('express');
+var express = require('express');
+var path = require('path');
+var app = express();
+
+var moment = require('moment');
+
+
+app.get('/', function(req, res){
+    res.sendFile(path.join(__dirname+'/index.html'));
+});
+
+var port = process.env.PORT || 8080; 
+
+app.listen(port, function(){
+    console.log('supposed to be ready on port 8080');
+});
+
+
+app.get('/:datestring', function(req,res) {
+  var myDate;
+  if(/^\d{8,}$/.test(req.params.datestring)) {
+    myDate = moment(req.params.datestring, "X");
+  } else {
+    myDate = moment(req.params.datestring, "MMMM D, YYYY");
+  }
+
+  if(myDate.isValid()) {
+    res.json({
+      unix: myDate.format("X"),
+      natural: myDate.format("MMMM D, YYYY")
+    });
+  } else {
+    res.json({
+      unix: null,
+      natural: null
+    });
+  }
+  
+});
